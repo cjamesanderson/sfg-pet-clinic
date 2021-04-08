d2408ddcb4bb48e87f33d10a6dc27cae616689d0
Cameron Anderson <cameron.anderson.j@gmail.com>
2021-04-08 15:49:21 -0400

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDare;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDare() {
+        return birthDare;
+    }
+
+    public void setBirthDare(LocalDate birthDare) {
+        this.birthDare = birthDare;
+    }
+}
