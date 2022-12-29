0910206caa6038a228c675d0e8e5cdb2e562a2ee
Cameron Anderson <cameron.anderson.j@gmail.com>
2022-12-28 20:27:30 -0800

    Adding POJOs. Closes #2

---
+++ src/main/java/model/Pet.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private LocalDate birthDate;
+    private PetType type;
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    private Owner owner;
+}
