51b1a43f651cc57cc14acc71aba3251102e097df
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-01-30 17:23:57 -0800

    Updated Pet to use JPA. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -9,12 +11,17 @@ public class Pet extends NamedEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
+
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public LocalDate getBirthDate() {
         return birthDate;
